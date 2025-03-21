97e69d041a523526b119af2f30b2a9b6e7a98388
John Uel Mangaba <johnuelmangaba@gmail.com>
2025-03-21 21:37:06 +0800

    add viewport

--- app/layout.js
+++ app/layout.js
@@ -56,6 +56,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
